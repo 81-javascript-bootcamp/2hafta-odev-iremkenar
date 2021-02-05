ce433637b88643a1914821a1600bb828be9a0269
Irem Kenar <irem.kenar@outlook.com>
2021-02-05 17:17:01 +0000

    Task completed.

--- odev.js
+++ odev.js
@@ -13,25 +13,42 @@ var car = {
     }
 }
 
-var myCarDetails =  car.displayDetails;
+/* Hocam merhaba, uc farkli metodla sonuca ulasabildim. Asagida sirasiyla bulabilirsiniz. Biri digerinden daha iyi midir emin degilim. 3 nolu metod aklima ilk gelendi fakat onu kullanmak icin myCarDetails fonksiyonunu silmem gerektigi icin onceliklendirmedim.*/
+/* Metod 1 : Explicit binding - bind. */  
+var myCarDetails =  car.displayDetails.bind(car);
 myCarDetails();
+// Metod 2: Explicit binding - call
+//var myCarDetails =  car.displayDetails;
+//myCarDetails.call(car);
+/* Metod 3 : implicit binding
+"var myCarDetails =  car.displayDetails;
+myCarDetails();" kod grubunun tamamini asagidaki kodla degistirebiliriz.*/
+//car.displayDetails();
+
 
 
 /** 
-
 2.name parametresi alan bir isValidName fonksiyonu yazin. Fonksiyon asagidaki kosullarin hepsi saglaniyorsa true, saglanmiyorsa false donmelidir:
 
 name string olmali
 name bos olmamali
-bosluk icerebilir, ancak bosluk haridcindeki isimler en az 2 karakterden olusmali.
+bosluk icerebilir, ancak bosluk haricindeki isimler en az 2 karakterden olusmali.
 
 **/
 
+
+/** 
+Bosluklari yok etmek için trim metodunu buldum, kalan string'in uzunlugunun 2 karakterden fazla olmasi gerektigi icin length ekledim. Trimden once String(Number(name)) denedim, derste Number(" 12ab ") uzerine konustugumuzda once bosluklari yok ettigine deginmistik. Fakat "X" testine hata verdigi icin trimle devam ettim.
+
+Hocam burada bir sorum var, typeof ve not equal operatorunu kullanirken strict ya da loose equality'nin sonuca etkisinden emin olamadim, cunku ikisinde de ayni sonuca ulasabiliyorum. Bu problem kapsaminda bir farki var midir? 
+**/  
+
 function isValidName(name) {
-  /// your code here
+    if (typeof name === 'string' && name !== "" && name.trim().length >= 2) {
+      return true;
+  } 
+  return false;
 }
-
-
 
 /**
 
@@ -50,6 +67,8 @@ function summary(genre, year) {
     `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`,
   )
 }
+/*Burada kodu dogru konsolda test etmek uzerine cok degerli bir ders ogrendim*/
+summary.call(book, 'dystopian', 1932);
 
 
 
